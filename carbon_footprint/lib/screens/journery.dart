699930804a66022d05487fd5501c81fe23c9783c
Amogh Prabhu <amoghprabhu@hotmail.com>
2021-12-07 18:12:07 +0530

    Modified journey page

--- carbon_footprint/lib/screens/journery.dart
+++ carbon_footprint/lib/screens/journery.dart
@@ -1,6 +1,10 @@
-import 'package:carbon_footprint/constants/themes.dart';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+
+import 'package:carbon_footprint/constants/themes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class JourneyCounter extends StatefulWidget {
   JourneyCounter({Key? key}) : super(key: key);
@@ -73,14 +77,87 @@ class _JourneyCounterState extends State<JourneyCounter> {
                 print("$distance");
               }
               prevLocation = snapshot.data as Position?;
-              return Center(
-                child: Text("$distance"),
+              return Scaffold(
+                backgroundColor: Colors.transparent,
+                body: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Your journey data:",
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                      SizedBox(height: 10),
+                      Expanded(
+                        child: ListView(
+                          children: [
+                            CustomContainer(
+                                image:
+                                    "https://image.shutterstock.com/image-illustration/city-map-pin-pointers-3d-260nw-1566197275.jpg",
+                                text: "Total distance travelled",
+                                value: distance),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               );
             } else {
-              print(snapshot);
               return Center(child: CircularProgressIndicator());
             }
           },
+        ),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  String image = "";
+  String text = "";
+  double value = 0;
+  CustomContainer({
+    Key? key,
+    required this.image,
+    required this.text,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 300,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Image.network(
+                image,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                text + ": $value",
+                style: GoogleFonts.ptSerif(
+                    color: kTextColor.withOpacity(0.8),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ))
+          ],
         ),
       ),
     );
