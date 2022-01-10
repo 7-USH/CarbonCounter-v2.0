313ad3f0494610c13b02082d3e6c036c9a2ed457
Tushar <tush1245@gmail.com>
2022-01-10 21:24:22 +0530

    merged files from soumi

--- carbon_footprint/lib/screens/journery.dart
+++ carbon_footprint/lib/screens/journery.dart
@@ -1,17 +1,12 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:carbon_footprint/models/Indicator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
-import 'package:carbon_footprint/constants/themes.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:velocity_x/src/flutter/shimmer.dart';
+import 'package:carbon_footprint/constants/themes.dart';
 
 class JourneyCounter extends StatefulWidget {
-  JourneyCounter({Key? key}) : super(key: key);
+  const JourneyCounter({Key? key}) : super(key: key);
   static String id = "journey";
 
   @override
@@ -19,9 +14,10 @@ class JourneyCounter extends StatefulWidget {
 }
 
 class _JourneyCounterState extends State<JourneyCounter> {
-  Position? _currentLocation;
   double distance = 0;
   Position? prevLocation;
+  bool isVisibile = false;
+  String vehicleType = "";
 
   @override
   void initState() {
@@ -33,98 +29,165 @@ class _JourneyCounterState extends State<JourneyCounter> {
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
+    final size = MediaQuery.of(context).size;
+    vehicleType = "Car";
+
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient:
-                LinearGradient(colors: [kPrimeColor, kGreenOne, kGreenTwo])),
-        child: StreamBuilder(
-          stream: Geolocator.getPositionStream(
-            locationSettings: LocationSettings(
-                accuracy: LocationAccuracy.best, distanceFilter: 100),
-          ),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (prevLocation != null) {
-                Position? location = (snapshot.data as Position?);
-                distance += Geolocator.distanceBetween(
-                    prevLocation!.latitude,
-                    prevLocation!.longitude,
-                    location!.latitude,
-                    location.longitude);
-              }
-              prevLocation = snapshot.data as Position?;
-              return Scaffold(
-                backgroundColor: Colors.transparent,
-                body: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: CircleAvatar(
-                          radius: 35,
-                          backgroundColor: kGreenOne,
-                          // ignore: prefer_const_constructors
+      body: StreamBuilder(
+        stream: Geolocator.getPositionStream(
+          locationSettings: const LocationSettings(
+              accuracy: LocationAccuracy.best, distanceFilter: 100),
+        ),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (prevLocation != null) {
+              Position? location = (snapshot.data as Position?);
+              distance += Geolocator.distanceBetween(
+                  prevLocation!.latitude,
+                  prevLocation!.longitude,
+                  location!.latitude,
+                  location.longitude);
+            }
+            prevLocation = snapshot.data as Position?;
+            return Stack(
+              children: [
+                GoogleMap(
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: false,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                      (snapshot.data as Position).latitude,
+                      (snapshot.data as Position).longitude,
+                    ),
+                    zoom: 15,
+                    tilt: 30,
+                  ),
+                  onTap: (LatLng loc) {
+                    setState(() {
+                      isVisibile = !isVisibile;
+                    });
+                  },
+                  mapType: MapType.normal,
+                ),
+                Positioned(
+                  top: 70,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(12),
+                    margin: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(100),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 10,
+                          offset: Offset.zero,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: kPrimeColor,
                           child: CircleAvatar(
-                            radius: 30,
                             backgroundImage: NetworkImage(user.photoURL!),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        "Journey Details",
-                        textScaleFactor: 1.5,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                      const SizedBox(height: 0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                user.displayName!,
+                                style: const TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Vehcile: " + vehicleType,
+                                style: const TextStyle(fontSize: 14),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                AnimatedPositioned(
+                  right: 0,
+                  left: 0,
+                  top: !isVisibile
+                      ? size.height
+                      : size.height - (size.height / 4),
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  child: Container(
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 10,
+                          offset: Offset.zero,
+                        ),
+                      ],
+                    ),
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
                         children: [
-                          Text(
-                            "$CarbonEmission",
-                            textScaleFactor: 2.5,
-                            style: Theme.of(context).textTheme.headline5,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SizedBox(
+                                      height: 40,
+                                      child: Image.network(
+                                          "https://cdn-icons-png.flaticon.com/512/1196/1196775.png"),
+                                    ),
+                                  ),
+                                  Text("Distance: " + distance.toString())
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SizedBox(
+                                      height: 40,
+                                      child: Image.network(
+                                          "https://icon-library.com/images/co2-icon/co2-icon-8.jpg"),
+                                    ),
+                                  ),
+                                  Text(
+                                    "Footprint: " + 100.toString(),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                          Text(
-                            " CO₂eKg",
-                            textScaleFactor: 1.5,
-                            style: Theme.of(context).textTheme.bodyText1,
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 25),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: CustomContainer(
-                              text: "Distance",
-                              value: distance,
-                              image:
-                                  "https://cdn-icons-png.flaticon.com/512/1196/1196775.png",
-                            ),
+                          const SizedBox(
+                            height: 28,
                           ),
-                          Expanded(
-                            child: CustomContainer(
-                              text: "Footprint",
-                              value: CarbonEmission,
-                              image:
-                                  "https://icon-library.com/images/co2-icon/co2-icon-8.jpg",
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Container(
+                          Container(
+                            width: 200,
+                            height: 50,
                             decoration: BoxDecoration(
-                              color: const Color.fromRGBO(255, 255, 255, 0.7),
-                              border: Border.all(color: kPrimeColor, width: 10),
+                              borderRadius: BorderRadius.circular(25),
+                              color: kGreenOne,
                               boxShadow: [
                                 BoxShadow(
                                   color: Colors.black.withOpacity(0.19),
@@ -133,86 +196,25 @@ class _JourneyCounterState extends State<JourneyCounter> {
                                   blurRadius: 8,
                                 ),
                               ],
-                              borderRadius: BorderRadius.circular(25),
                             ),
-                            child: Container(
-                              clipBehavior: Clip.antiAlias,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              child: GoogleMap(
-                                initialCameraPosition: CameraPosition(
-                                  target: LatLng(
-                                    (snapshot.data as Position).latitude,
-                                    (snapshot.data as Position).longitude,
-                                  ),
-                                  zoom: 15,
-                                ),
-                                mapType: MapType.normal,
+                            child: const Center(
+                              child: Text(
+                                "End Journery",
+                                style: TextStyle(color: Colors.white),
                               ),
                             ),
-                          ),
-                        ),
-                      )
-                    ],
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              );
-            } else {
-              return Indicator();
-            }
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class CustomContainer extends StatelessWidget {
-  String image = "";
-  String text = "";
-  double value = 0;
-  CustomContainer({
-    Key? key,
-    required this.text,
-    required this.value,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: 100,
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: const Color.fromRGBO(255, 255, 255, 0.7),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.19),
-                  offset: const Offset(1, 10),
-                  spreadRadius: 1,
-                  blurRadius: 8),
-            ],
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: Row(
-            children: [
-              Expanded(child: Image.network(image)),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    Expanded(child: FittedBox(child: Text(text))),
-                    Expanded(child: Text(value.toString())),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+                )
+              ],
+            );
+          } else {
+            return Indicator();
+          }
+        },
       ),
     );
   }
